9b1ce0f1a90dc59720ec9eba64c092c617083e66
Jack Pena <penajackc@gmail.com>
2025-12-02 09:51:54 -0600

    intial setup, math to come

--- 2025/lib/aoc2025/y25/day02.ex
+++ 2025/lib/aoc2025/y25/day02.ex
@@ -2,30 +2,35 @@ defmodule Aoc2025.Y25.Day02 do
   alias AoC.Input
 
   def parse(input, _part) do
-    # This function will receive the input path or an %AoC.Input.TestInput{}
-    # struct. To support the test you may read both types of input with either:
-    #
-    # * Input.stream!(input), equivalent to File.stream!/1
-    # * Input.stream!(input, trim: true), equivalent to File.stream!/2
-    # * Input.read!(input), equivalent to File.read!/1
-    #
-    # The role of your parse/2 function is to return a "problem" for the solve/2
-    # function.
-    #
-    # For instance:
-    #
-    # input
-    # |> Input.stream!()
-    # |> Enum.map!(&my_parse_line_function/1)
-
     Input.read!(input)
   end
 
   def part_one(problem) do
-    # This function receives the problem returned by parse/2 and must return
-    # today's problem solution for part one.
+    total =
+      problem
+      # for testing
+      |> String.trim()
+      |> String.split(",")
+      |> Enum.reject(fn x -> x == "" end)
+      |> Enum.reduce(0, fn range, acc ->
+        range_endings = String.split(range, "-")
 
-    problem
+        if disqualified_range?(range_endings) do
+          IO.puts("BAD RANGE")
+          acc
+        else
+          IO.puts("Need to check this range")
+        end
+      end)
+
+    total
+  end
+
+  defp disqualified_range?([first, last]) do
+    len_first = String.length(first)
+    len_last = String.length(last)
+
+    len_first == len_last and rem(len_first, 2) == 1
   end
 
   # def part_two(problem) do
--- 2025/test/2025/day02_test.exs
+++ 2025/test/2025/day02_test.exs
@@ -31,14 +31,10 @@ defmodule Aoc2025.Y25.Day02Test do
 
   test "part one example" do
     input = ~S"""
-    This is an
-    example input.
-    replace with
-    an example from
-    the AoC website.
+    11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
     """
 
-    assert CHANGE_ME == solve(input, :part_one)
+    assert 1_227_775_554 == solve(input, :part_one)
   end
 
   # Once your part one was successfully sumbitted, you may uncomment this test
