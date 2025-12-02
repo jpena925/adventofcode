6293beff583cbaad79ca82e0e8d44c041d08ccec
Jack Pena <penajackc@gmail.com>
2025-12-02 15:39:45 -0600

    not eloquent day 2 part 1. thankful elixir is fast

--- 2025/lib/aoc2025/y25/day02.ex
+++ 2025/lib/aoc2025/y25/day02.ex
@@ -8,7 +8,6 @@ defmodule Aoc2025.Y25.Day02 do
   def part_one(problem) do
     total =
       problem
-      # for testing
       |> String.trim()
       |> String.split(",")
       |> Enum.reject(fn x -> x == "" end)
@@ -16,10 +15,9 @@ defmodule Aoc2025.Y25.Day02 do
         range_endings = String.split(range, "-")
 
         if disqualified_range?(range_endings) do
-          IO.puts("BAD RANGE")
           acc
         else
-          IO.puts("Need to check this range")
+          acc + check_within_range(range_endings)
         end
       end)
 
@@ -31,6 +29,26 @@ defmodule Aoc2025.Y25.Day02 do
     len_last = String.length(last)
 
     len_first == len_last and rem(len_first, 2) == 1
+  end
+
+  defp check_within_range([first, last]) do
+    String.to_integer(first)..String.to_integer(last)
+    |> Enum.reduce(0, fn num, acc ->
+      num_string = Integer.to_string(num)
+
+      if is_repeated?(num_string), do: acc + num, else: acc
+    end)
+  end
+
+  defp is_repeated?(num_string) do
+    len = String.length(num_string)
+
+    if rem(len, 2) == 1 do
+      false
+    else
+      {left, right} = String.split_at(num_string, div(len, 2))
+      left == right
+    end
   end
 
   # def part_two(problem) do
