029550f49be6f67af802f977c0cbaf9554993e16
Jack Pena <penajackc@gmail.com>
2025-12-03 12:29:43 -0600

    day 3 part 1

--- 2025/lib/aoc2025/y25/day03.ex
+++ 2025/lib/aoc2025/y25/day03.ex
@@ -2,30 +2,33 @@ defmodule Aoc2025.Y25.Day03 do
   alias AoC.Input
 
   def parse(input, _part) do
-    # This function will receive the input path or an %AoC.Input.TestInput{}
-    # struct. To support the test you may read both types of input with either:
-    #
-    # * Input.stream!(input), equivalent to File.stream!/1
-    # * Input.stream!(input, trim: true), equivalent to File.stream!/2
-    # * Input.read!(input), equivalent to File.read!/1
-    #
-    # The role of your parse/2 function is to return a "problem" for the solve/2
-    # function.
-    #
-    # For instance:
-    #
-    # input
-    # |> Input.stream!()
-    # |> Enum.map!(&my_parse_line_function/1)
-
     Input.read!(input)
   end
 
   def part_one(problem) do
-    # This function receives the problem returned by parse/2 and must return
-    # today's problem solution for part one.
-
     problem
+    |> String.split("\n", trim: true)
+    |> Enum.reduce(0, fn line, acc ->
+      acc + get_largest_two_digits(line)
+    end)
+  end
+
+  defp get_largest_two_digits(line) do
+    {_curr_max, right_maxes} =
+      line
+      |> String.reverse()
+      |> String.to_charlist()
+      |> Enum.reduce({0, []}, fn num, {running_max, list} ->
+        {max(running_max, num), [running_max | list]}
+      end)
+
+    original = String.to_charlist(line)
+
+    right_maxes
+    |> Enum.drop(-1)
+    |> Enum.zip(original)
+    |> Enum.map(fn {m, d} -> (d - ?0) * 10 + (m - ?0) end)
+    |> Enum.max()
   end
 
   # def part_two(problem) do
