3599b337fc29aa7e4952c6ee07f6cb29513fbc70
Jack Pena <penajackc@gmail.com>
2025-12-08 16:42:22 -0600

    day7 part2 much easier than p1

--- 2025/lib/aoc2025/y25/day08.ex
+++ 2025/lib/aoc2025/y25/day08.ex
@@ -70,7 +70,45 @@ defmodule Aoc2025.Y25.Day08 do
     end
   end
 
-  # def part_two(problem) do
-  #   problem
-  # end
+  def part_two(problem) do
+    points =
+      problem
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn point ->
+        [x, y, z] = String.split(point, ",")
+        {String.to_integer(x), String.to_integer(y), String.to_integer(z)}
+      end)
+
+    sorted_pairs =
+      points
+      |> all_pairs()
+      |> Enum.map(fn pair -> calculate_distances(pair) end)
+      |> Enum.sort()
+
+    parents_map = Map.new(points, fn point -> {point, point} end)
+    num_circuits = length(points)
+
+    {_final_map, _circuits, last_p1, last_p2} =
+      Enum.reduce_while(sorted_pairs, {parents_map, num_circuits, nil, nil}, fn {_dist, point1, point2}, {pmap, circuits, _, _} ->
+        root1 = find_root(point1, pmap)
+        root2 = find_root(point2, pmap)
+
+        if root1 != root2 do
+          new_pmap = Map.put(pmap, root1, root2)
+          new_circuits = circuits - 1
+
+          if new_circuits == 1 do
+            {:halt, {new_pmap, new_circuits, point1, point2}}
+          else
+            {:cont, {new_pmap, new_circuits, point1, point2}}
+          end
+        else
+          {:cont, {pmap, circuits, nil, nil}}
+        end
+      end)
+
+    {x1, _, _} = last_p1
+    {x2, _, _} = last_p2
+    x1 * x2
+  end
 end
