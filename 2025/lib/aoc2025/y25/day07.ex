a42a3c02fcb1d31ef246c1c5badebf8b2e5fd09c
Jack Pena <penajackc@gmail.com>
2025-12-08 08:48:37 -0600

    day7part1

--- 2025/lib/aoc2025/y25/day07.ex
+++ 2025/lib/aoc2025/y25/day07.ex
@@ -2,30 +2,55 @@ defmodule Aoc2025.Y25.Day07 do
   alias AoC.Input
 
   def parse(input, _part) do
-    # This function will receive the input path or an %AoC.Input.TestInput{}
-    # struct. To support the test you may read both types of input with either:
-    #
-    # * Input.stream!(input), equivalent to File.stream!/1
-    # * Input.stream!(input, trim: true), equivalent to File.stream!/2
-    # * Input.read!(input), equivalent to File.read!/1
-    #
-    # The role of your parse/2 function is to return a "problem" for the solve/2
-    # function.
-    #
-    # For instance:
-    #
-    # input
-    # |> Input.stream!()
-    # |> Enum.map!(&my_parse_line_function/1)
-
-    Input.read!(input)
+    input
+    |> Input.read!()
+    |> String.split("\n", trim: true)
   end
 
-  def part_one(problem) do
-    # This function receives the problem returned by parse/2 and must return
-    # today's problem solution for part one.
+  def part_one(rows) do
+    [first_row | rest] = rows
+    s_col = find_char_index(first_row, "S")
 
-    problem
+    initial_beams = MapSet.new([s_col])
+
+    {splits, _final_beams} =
+      Enum.reduce(rest, {0, initial_beams}, fn row, {split_count, beams} ->
+        splitters = find_all_indices(row, "^")
+
+        hits = MapSet.intersection(beams, splitters)
+
+        new_splits = MapSet.size(hits)
+
+        new_beams =
+          beams
+          |> MapSet.difference(hits)
+          |> then(fn remaining ->
+            Enum.reduce(hits, remaining, fn hit_pos, acc ->
+              acc
+              |> MapSet.put(hit_pos - 1)
+              |> MapSet.put(hit_pos + 1)
+            end)
+          end)
+
+        {split_count + new_splits, new_beams}
+      end)
+
+    splits
+  end
+
+  defp find_char_index(row, char) do
+    row
+    |> String.graphemes()
+    |> Enum.find_index(&(&1 == char))
+  end
+
+  defp find_all_indices(row, char) do
+    row
+    |> String.graphemes()
+    |> Enum.with_index()
+    |> Enum.filter(fn {c, _idx} -> c == char end)
+    |> Enum.map(fn {_c, idx} -> idx end)
+    |> MapSet.new()
   end
 
   # def part_two(problem) do
