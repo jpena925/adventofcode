e6daf8350cb34572f80151708f699f9079f6c0dd
Jack Pena <penajackc@gmail.com>
2025-12-08 12:40:43 -0600

    d8p1 process into coordinates and distances

--- 2025/lib/aoc2025/y25/day08.ex
+++ 2025/lib/aoc2025/y25/day08.ex
@@ -6,10 +6,28 @@ defmodule Aoc2025.Y25.Day08 do
   end
 
   def part_one(problem) do
-    # This function receives the problem returned by parse/2 and must return
-    # today's problem solution for part one.
-
     problem
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn point ->
+      [x, y, z] = String.split(point, ",")
+      {String.to_integer(x), String.to_integer(y), String.to_integer(z)}
+    end)
+    |> all_pairs()
+    |> Enum.map(fn pair -> calculate_distances(pair) end)
+    |> Enum.sort()
+    |> IO.inspect()
+  end
+
+  defp all_pairs([]), do: []
+
+  defp all_pairs([head | tail]) do
+    Enum.map(tail, fn point ->
+      {head, point}
+    end) ++ all_pairs(tail)
+  end
+
+  defp calculate_distances({{x1, y1, z1}, {x2, y2, z2}} = {point1, point2}) do
+    {(x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2, point1, point2}
   end
 
   # def part_two(problem) do
