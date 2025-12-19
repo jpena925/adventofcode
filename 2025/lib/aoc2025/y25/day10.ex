2c12ac0cfdc46fd5be3c570f5a67662fc2a7a8dc
Jack Pena <penajackc@gmail.com>
2025-12-19 12:04:49 -0600

    parse everything

--- 2025/lib/aoc2025/y25/day10.ex
+++ 2025/lib/aoc2025/y25/day10.ex
@@ -2,30 +2,44 @@ defmodule Aoc2025.Y25.Day10 do
   alias AoC.Input
 
   def parse(input, _part) do
-    # This function will receive the input path or an %AoC.Input.TestInput{}
-    # struct. To support the test you may read both types of input with either:
-    #
-    # * Input.stream!(input), equivalent to File.stream!/1
-    # * Input.stream!(input, trim: true), equivalent to File.stream!/2
-    # * Input.read!(input), equivalent to File.read!/1
-    #
-    # The role of your parse/2 function is to return a "problem" for the solve/2
-    # function.
-    #
-    # For instance:
-    #
-    # input
-    # |> Input.stream!()
-    # |> Enum.map!(&my_parse_line_function/1)
-
     Input.read!(input)
   end
 
   def part_one(problem) do
-    # This function receives the problem returned by parse/2 and must return
-    # today's problem solution for part one.
+    problem
+    |> String.split("\n", trim: true)
+    |> parse_everything()
 
     problem
+  end
+
+  def parse_everything(lines) do
+    lines
+    |> Enum.map(fn line ->
+      {parse_pattern(line), parse_buttons(line)}
+    end)
+  end
+
+  def parse_pattern(line) do
+    [_, pattern] = Regex.run(~r/\[([.#]+)\]/, line)
+
+    pattern
+    |> String.graphemes()
+    |> Enum.map(fn
+      "." -> 0
+      "#" -> 1
+    end)
+  end
+
+  def parse_buttons(line) do
+    buttons = Regex.scan(~r/\(([0-9,]+)\)/, line)
+
+    buttons
+    |> Enum.map(fn [_, button] ->
+      button
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+    end)
   end
 
   # def part_two(problem) do
